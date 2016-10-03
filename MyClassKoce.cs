f6d953f087799c6bf20f86992e9f0de8b62b5f09
josiplabrovic <josip.labrovic@hotmail.com>
2016-10-03 11:54:08 +0200

    Added new class, fix

---
+++ MyClassKoce.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZW16
+{
+    class MyClassKoce
+    {
+        public static Int32 i32KolikoSiSpreman;
+    }
+}
